2c42c2e5349003553e35478719d513ceac8e97d9
Vadzim <stubeda0002@mail.ru>
2026-04-25 14:48:32 +0500

    refactor: delete extra dto

--- src/main/java/com/stubedavd/match/dto/response/FinalScoreResponseDto.java
+++
@@ -1,9 +0,0 @@
-package com.stubedavd.match.dto.response;
-
-import com.stubedavd.player.dto.response.PlayerFinalScoreResponseDto;
-
-public record FinalScoreResponseDto(
-        PlayerFinalScoreResponseDto player1FinalScoreResponseDto,
-        PlayerFinalScoreResponseDto player2FinalScoreResponseDto
-) {
-}
